59e593720c3639fffc5be46dddf18e4cddd7ed4f
Zdravka Strahinova <zdravka.strahinova@primeholding.com>
2022-01-14 15:02:16 +0200

    Add directives

--- ng-bookshop-app/src/app/app.module.ts
+++ ng-bookshop-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
